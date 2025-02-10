31136dcfe43ac2c1b151bac4c8e961978edb0fbb
Jashan-Windows <jashanbhullar376@gmail.com>
2025-02-10 22:50:05 +0530

    feat(frontend): add protected route

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router"
+import api from "../api"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+import { jwtDecode } from "jwt-decode"
+
+const ProtectedRoute = ({ children }) => {
+    const [isAuthorized, setIsAuthorized] = useState(null)
+    let navigate = useNavigate()
+
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false))
+    }, [])
+
+    const refreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try {
+            const res = await api.post("/api/token/refresh/", {
+                refresh: refreshToken,
+            })
+            if (res.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                setIsAuthorized(true)
+            } else {
+                setIsAuthorized(false)
+            }
+        } catch (error) {
+            console.log(error)
+            setIsAuthorized(false)
+        }
+    }
+
+    const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN)
+        if (!token) {
+            setIsAuthorized(false)
+            return
+        }
+        const decoded = jwtDecode(token)
+        const tokenExpiration = decoded.exp
+        const now = Date.now() / 1000
+
+        if (tokenExpiration < now) {
+            setIsAuthorized(false)
+            await refreshToken()
+        } else {
+            setIsAuthorized(true)
+        }
+    }
+
+    if (isAuthorized === null) {
+        return <div>Loading...</div>
+    }
+
+    return isAuthorized ? children : navigate("/login")
+}
+
+export default ProtectedRoute
