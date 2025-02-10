2675fdd36c015ca30bf98db6e2213324975f6252
Jashan-Windows <jashanbhullar376@gmail.com>
2025-02-10 23:21:03 +0530

    feat(frontend): setup basic page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from "react"
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router"
+import { Home } from "./pages/Home"
+import { Login } from "./pages/Login"
+import { NotFound } from "./pages/NotFound"
+import { Register } from "./pages/Register"
+import ProtectedRoute from "./components/ProtectedRoute"
 
-export default App = () => {
-    const [count, setCount] = useState(0)
+const Logout = () => {
+    let navigate = useNavigate()
+    localStorage.clear()
+    return navigate("/login")
+}
 
+const RegisterAndLogout = () => {
+    localStorage.clear()
+    return <Register />
+}
+
+export const App = () => {
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<RegisterAndLogout />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
